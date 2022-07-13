a7854c7887d78d7dc737b1747b59add4e5e5ccba
Aron Ilie <aronilie.code@gmail.com>
2022-07-13 20:40:41 +0200

    Debugged calculadoraHTML exercise

--- public/calculadoraHTML/index.js
+++ public/calculadoraHTML/index.js
@@ -1,30 +1,24 @@
-let display = document.getElementById('display');
+const display = document.getElementById("display");
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+const buttons = Array.from(document.getElementsByClassName("button"));
 
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText) {
-            case 'AC':
-                display.innerText = '';
-                break;
-            case 'Del':
-                if (display.innerText) {
-                    display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            case '=':
-                console.log(display.innerText);
-                display.innerText = 5;
-                break;
-            default:
-                display.innerText += e.target.innerText;
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "AC":
+        display.innerText = "";
+        break;
+      case "Del":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
         }
-    })
-})
-
-function calculate (string) {
-
-
-    return result;
-}
+        break;
+      case "=":
+        console.log(display.innerText);
+        display.innerText = 5;
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
